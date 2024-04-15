394c82feeb5abdc20beaa27a6be5d9f1f5d7f569
GabrielCervantes15 <125184081+GabrielCervantes15@users.noreply.github.com>
2024-04-14 21:20:55 -0700

    Proyecto Inicial
    Ultimo proyecto subido a la plataforma Ivirtual

---
+++ src/main/java/com/mycompany/zoologicods/ZoologicoDS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.zoologicods;
+import com.mycompany.zoologicods.frmMenu;
+/**
+ *
+ * @author Gabriel
+ */
+public class ZoologicoDS {
+
+    public static void main(String[] args) {
+      frmMenu menu=new frmMenu();
+      menu.setVisible(true);
+    }
+}
