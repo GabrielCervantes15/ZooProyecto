6c730b66114552f1b84b210bfec3c8a0e574cf94
GabrielCervantes15 <125184081+GabrielCervantes15@users.noreply.github.com>
2024-04-23 20:44:31 -0700

    proyectodd
    dfaf

---
+++ src/main/java/Presentacion/ZoologicoDS.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Presentacion;
+import Presentacion.frmMenu;
+/**
+ *
+ * @author Gabriel
+ */
+public class ZoologicoDS {
+
+    public static void main(String[] args) {
+      frmMenu menu=new frmMenu();
+      menu.setVisible(true);
+    }
+}
